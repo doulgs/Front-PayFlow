42dd3f99c386b2d0b3056ad2193fb3ff0a2d5c09
doulgs <douglasdsdomiciano@hotmail.com>
2025-06-09 23:15:28 -0300

    feat: adicionar suporte ao BottomSheet e otimizar a exibição de transações recentes

--- src/layouts/latest-transactions/index.tsx
+++ src/layouts/latest-transactions/index.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import { ActivityIndicator, TouchableOpacity, View } from "react-native";
-
 import { Card } from "@/components/ui/cards";
+import { BottomSheetRef, useBottomSheet } from "@/hooks/useBottomSheet";
 import { useChangeLanguage } from "@/hooks/useChangeLanguage";
 import { useCurrency } from "@/hooks/useCurrency";
 import { useTheme } from "@/hooks/useTheme";
@@ -10,6 +8,8 @@ import { colors } from "@/styles/colors";
 import { FinanceTransaction } from "@/types/finance";
 import { clsx } from "clsx";
 import { LayoutList } from "lucide-react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { ActivityIndicator, TouchableOpacity, View } from "react-native";
 
 import ImageNoData from "@/assets/images/research-paper-amico.svg";
 
@@ -26,95 +26,123 @@ const LatestTransactions: React.FC<LatestTransactionsProps> = ({ data, isLoading
   const { formatCurrency } = useCurrency();
   const { isVisible } = useVisibilityStore();
 
+  const bottomSheetRef = useRef<BottomSheetRef>(null);
+  const [selectedTransaction, setSelectedTransaction] = useState<FinanceTransaction | null>(null);
+
+  const { BottomSheet, open } = useBottomSheet(bottomSheetRef);
+
+  const handleOpenDetails = (item: FinanceTransaction) => {
+    setSelectedTransaction({ ...item });
+  };
+
+  useEffect(() => {
+    if (selectedTransaction) open();
+  }, [selectedTransaction]);
+
   const hasNoData = !isLoading && data.length === 0;
 
   return (
-    <Card variant="outlined">
-      <Card.Header className="items-center px-1 gap-2">
-        <Card.Icon>
-          <LayoutList size={20} color={currentTheme === "dark" ? "white" : "black"} />
-        </Card.Icon>
-        <Card.Text className="text-lg font-semibold">{t("finance.latest.title")}</Card.Text>
-      </Card.Header>
+    <>
+      <Card variant="outlined">
+        <Card.Header className="items-center px-1 gap-2">
+          <Card.Icon>
+            <LayoutList size={20} color={currentTheme === "dark" ? "white" : "black"} />
+          </Card.Icon>
+          <Card.Text className="text-lg font-semibold">{t("finance.latest.title")}</Card.Text>
+        </Card.Header>
 
-      {isLoading ? (
-        <Card.Body className="items-center justify-center my-6">
-          <ActivityIndicator size="large" color={colors.light.brand.primary} />
-          <Card.Text className="mt-2 text-sm text-muted">{t("finance.latest.loading")}</Card.Text>
-        </Card.Body>
-      ) : hasNoData ? (
-        <Card.Body className="items-center justify-center my-4">
-          <ImageNoData width={200} height={200} />
-          <Card.Footer className="mt-2">
-            <Card.Text>{t("finance.latest.empty")}</Card.Text>
-          </Card.Footer>
-        </Card.Body>
-      ) : (
-        <>
-          <Card.Body>
-            <Card.Map
-              variant="ghost"
-              className="px-1"
-              items={data.slice(0, RECORD_DISPLAY_LIMIT)}
-              renderItem={(item) => (
-                <TouchableOpacity key={item.id} onPress={() => console.log(`Ver detalhes de: ${item.id}`)}>
-                  <Card.Body className="flex-row items-center justify-between gap-2">
-                    <Card.Icon variant="outlined">
-                      <Card.Text>
-                        {item.title
-                          .split(" ")
-                          .slice(0, 2)
-                          .map((word) => word.charAt(0).toUpperCase())
-                          .join("")}
-                      </Card.Text>
-                    </Card.Icon>
-
-                    <View className="flex-1 mr-4">
-                      <Card.Text
-                        numberOfLines={1}
-                        className={clsx(
-                          "text-md font-bold",
-                          item.status === "paid" && "text-zinc-300 dark:text-zinc-600 line-through"
-                        )}
-                      >
-                        {item.title}
-                      </Card.Text>
-                      <Card.Text
-                        numberOfLines={1}
-                        className={clsx(
-                          "text-sm",
-                          item.status === "paid" && "text-zinc-300 dark:text-zinc-600 line-through"
-                        )}
-                      >
-                        {item.description}
-                      </Card.Text>
-                    </View>
-
-                    <View className="items-end">
-                      <Card.Text
-                        numberOfLines={1}
-                        className={clsx(
-                          "text-lg font-bold",
-                          item.status === "paid" && "text-zinc-300 dark:text-zinc-600 line-through"
-                        )}
-                      >
-                        {isVisible ? "*******" : formatCurrency(item.value)}
-                      </Card.Text>
-                    </View>
-                  </Card.Body>
-                </TouchableOpacity>
-              )}
-            />
+        {isLoading ? (
+          <Card.Body className="items-center justify-center my-6">
+            <ActivityIndicator size="large" color={colors.light.brand.primary} />
+            <Card.Text className="mt-2 text-sm text-muted">{t("finance.latest.loading")}</Card.Text>
           </Card.Body>
-
-          {data.length >= RECORD_DISPLAY_LIMIT && (
-            <Card.Footer className="items-center justify-center">
-              <Card.Text>{t("finance.latest.see-more")}</Card.Text>
+        ) : hasNoData ? (
+          <Card.Body className="items-center justify-center my-4">
+            <ImageNoData width={200} height={200} />
+            <Card.Footer className="mt-2">
+              <Card.Text>{t("finance.latest.empty")}</Card.Text>
             </Card.Footer>
-          )}
-        </>
-      )}
-    </Card>
+          </Card.Body>
+        ) : (
+          <>
+            <Card.Body>
+              <Card.Map
+                variant="ghost"
+                className="px-1"
+                items={data.slice(0, RECORD_DISPLAY_LIMIT)}
+                renderItem={(item) => (
+                  <TouchableOpacity key={item.id} onPress={() => handleOpenDetails(item)}>
+                    <Card.Body className="flex-row items-center justify-between gap-2">
+                      <Card.Icon variant="outlined">
+                        <Card.Text>
+                          {item.title
+                            .split(" ")
+                            .slice(0, 2)
+                            .map((word) => word.charAt(0).toUpperCase())
+                            .join("")}
+                        </Card.Text>
+                      </Card.Icon>
+
+                      <View className="flex-1 mr-4">
+                        <Card.Text
+                          numberOfLines={1}
+                          className={clsx(
+                            "text-md font-bold",
+                            item.status === "paid" && "text-zinc-300 dark:text-zinc-600 line-through"
+                          )}
+                        >
+                          {item.title}
+                        </Card.Text>
+                        <Card.Text
+                          numberOfLines={1}
+                          className={clsx(
+                            "text-sm",
+                            item.status === "paid" && "text-zinc-300 dark:text-zinc-600 line-through"
+                          )}
+                        >
+                          {item.description}
+                        </Card.Text>
+                      </View>
+
+                      <View className="items-end">
+                        <Card.Text
+                          numberOfLines={1}
+                          className={clsx(
+                            "text-lg font-bold",
+                            item.status === "paid" && "text-zinc-300 dark:text-zinc-600 line-through"
+                          )}
+                        >
+                          {isVisible ? "*******" : formatCurrency(item.value)}
+                        </Card.Text>
+                      </View>
+                    </Card.Body>
+                  </TouchableOpacity>
+                )}
+              />
+            </Card.Body>
+
+            {data.length > RECORD_DISPLAY_LIMIT && (
+              <Card.Footer className="items-center justify-center">
+                <Card.Text>{t("finance.latest.see-more")}</Card.Text>
+              </Card.Footer>
+            )}
+          </>
+        )}
+      </Card>
+
+      <BottomSheet
+        title={t("common.greeting.afternoon")}
+        render={
+          selectedTransaction && (
+            <View className="gap-4 p-2">
+              <Card.Text className="text-xl font-bold">{selectedTransaction.title}</Card.Text>
+              <Card.Text>{selectedTransaction.description}</Card.Text>
+              <Card.Text>{formatCurrency(selectedTransaction.value)}</Card.Text>
+            </View>
+          )
+        }
+      />
+    </>
   );
 };
 
